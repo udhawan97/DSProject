7e6e2c707f751a1b17258b6b0e1ebd18f540bc2f
devinorman7 <70539222+devinorman7@users.noreply.github.com>
2020-10-14 14:57:15 -0400

    get and post updates for members

---
+++ app/public/api/members/post.php
@@ -0,0 +1,32 @@
+<?php
+
+require '/srv/app/common.php';
+
+// Step 0: Validate the incoming data
+// This code doesn't do that, but should ...
+// For example, if the date is empty or bad, this insert fails.
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+// Note the use of parameterized statements to avoid injection
+$stmt = $db->prepare(
+  'INSERT INTO people (personID, firstName, lastName,)
+  VALUES (?, ?, ?)'
+);
+
+$stmt->execute([
+  $_POST['patientGuid'],
+  $_POST['firstName'],
+  $_POST['LastName']
+]);
+
+// If needed, get auto-generated PK from DB
+// $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+
+// Step 4: Output
+// Here, instead of giving output, I'm redirecting to the SELECT API,
+// just in case the data changed by entering it
+header('HTTP/1.1 303 See Other');
+header('Location: ../members/');
