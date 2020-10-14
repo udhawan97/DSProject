60b45791358ce6e0fb718b2952d16f08876efe95
mzheng77 <70542040+mzheng77@users.noreply.github.com>
2020-10-14 12:12:22 -0400

    get changes for certification

---
+++ app/public/api/certifications/get.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM certification';
+$vars = [];
+
+if (isset($_GET['guid'])) {
+  // This is an example of a parameterized query
+  $sql = 'SELECT * FROM certification WHERE certifyID = ?';
+  $sql = 'SELECT * FROM certification WHERE certifyName = ?';
+  $sql = 'SELECT * FROM certification WHERE expirePeriod = ?';
+  $vars = [ $_GET['guid'] ];
+}
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$certifications = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($certifications, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
