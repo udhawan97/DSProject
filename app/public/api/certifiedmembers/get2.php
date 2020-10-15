a9c0c1027fe76cbfbd564cdeb788e24a694c0293
udhawan97 <70520984+udhawan97@users.noreply.github.com>
2020-10-15 15:08:12 -0500

    bug fixes on HTML pages
    fixed nav bar links
    fixed copyright info
    fixed uneven indentation and spaces

---
+++ app/public/api/certifiedmembers/get2.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM certifiedUsers';
+$vars = [];
+
+if (isset($_GET['guid'])) {
+  // This is an example of a parameterized query
+  $vars = [ $_GET['guid'] ];
+}
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$certifications = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($certifications, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
